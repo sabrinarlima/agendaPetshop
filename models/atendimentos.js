8b255966b793632ddde02d8975bc84ac54d8bf94
sabrinarlima <classesabrinarodrigues@gmail.com>
2021-01-04 18:42:59 -0300

    atualizaçao dos arquivos atendimentos em controllers e models

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,44 +1,106 @@
 const moment = require('moment')
+const atendimentos = require('../controllers/atendimentos')
 const conexao = require('../infraestrutura/conexao')
 
 class Atendimento {
     adiciona(atendimento, res) {
         const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
         const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
-        
-        const dataValida = moment(data).isSameOrAfter(dataCriacao) 
-        const cliValido = atendimento.cliente.length >=5
+
+        const dataValida = moment(data).isSameOrAfter(dataCriacao)
+        const cliValido = atendimento.cliente.length >= 5
 
         const validacoes = [
-        {
-            nome: 'data',
-            valido:dataValida,
-            mensagem: 'Data deve ser maior ou igual a data atual'
-        },
-        {
-            nome: 'cliente',
-            valido: cliValido,
-            mensagem: 'Cliente deve ter pelo menos 5 caracteres'
+            {
+                nome: 'data',
+                valido: dataValida,
+                mensagem: 'Data deve ser maior ou igual a data atual'
+            },
+            {
+                nome: 'cliente',
+                valido: cliValido,
+                mensagem: 'Cliente deve ter pelo menos 5 caracteres'
+            }
+        ]
+
+        const erros = validacoes.filter(campo => !campo.valido)
+        const existemErros = erros.length
+
+        if (existemErros) {
+            res.status(400).json(erros)
+        } else {
+
+            const atendimentoDatado = { ...atendimento, dataCriacao, data }
+            const sql = 'INSERT INTO Atendimentos SET ?'
+
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if (erro) {
+                    res.status(400).json(erro)
+                } else {
+                    res.status(201).json(resultados)
+                }
+            })
         }
-    ]
+    }
 
-    const erros = validacoes.filter(campo => !campo.valido)
-    const existemErros = erros.length
+    lista(res) {
+        const sql = 'SELECT * FROM Atendimentos'
 
-    if(existemErros) {
-        res.status(400).json(erros)
-    }else{
-
-        const atendimentoDatado = {...atendimento, dataCriacao, data}
-        const sql = 'INSERT INTO Atendimentos SET ?'
-
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro) {
+        conexao.query(sql, (erro, resultados) => {
+            if (erro) {
                 res.status(400).json(erro)
             } else {
-                res.status(201).json(resultados)
+                res.status(200).json(resultados)
+            }
+
+        })
+    }
+
+    buscaPorId(id, res) {
+        const sql = `SELECT * FROM Atendimentos WHERE id=${id}`;
+
+        conexao.query(sql, (erro, resultados) => {
+            const atendimento = resultados.length > 0 ?
+                resultados[0] : {}
+            if (erro) {
+                res.status(400).json(erro)
+            } else {
+                res.status(200).json(atendimento)
+            }
+
+        })
+    }
+
+    altera(id, valores, res) {
+        if(valores.data) {
+            valores.data = moment(valores.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        }
+        const sql =  'UPDATE Atendimentos SET ? WHERE id=?'
+
+
+        conexao.query(sql, [valores, id], (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            }else {
+                res.status(200).json(resultados)
             }
         })
     }
-}}
+
+    deleta(id, res) {
+        const sql = 'DELETE FROM Atendimentos WHERE id=?'
+
+        conexao.query(aql, id, (erro, resultados) => {
+            if(erro) {
+                res.status(400).json(erro)
+            }else {
+                res.status(200).json(resultados)
+            }
+
+        })
+    }
+}
+
+
+
 module.exports = new Atendimento    
